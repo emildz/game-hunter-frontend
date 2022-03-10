35b69f055c6ccc580811565337ea20e21af946a4
Emil Dzhebrailov <emil.dzhebrailov@yahoo.com>
2022-03-10 13:51:32 -0500

    created components for app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// import { Layout } from "antd";
+// import { CreateGame } from './components/CreateGame'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <h1 style={{color: "black"}}>Game Hunter</h1>
+        {/* <Layout.Header
+          style={{
+            position: "fixed",
+            width: "100%",
+            display: "flex",
+            zIndex: "10",
+          }}
+        > */}
+        {/* </Layout.Header> */}
+        {/* <Routes>
+          <Route path="/" exact />
+          {/* <Route path="/create-game" element={<CreateGame />} /> 
+        </Routes> */}
+      </Router>
+    </>
   );
 }
 
